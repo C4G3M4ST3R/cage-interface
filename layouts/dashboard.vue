56550ccd33e24e23cbcaca18b12d702a29137fc7
Oluwaseyi Joshua <seyijosh44@gmail.com>
2021-08-26 22:14:00 +0100

    Added dashboard

---
+++ layouts/dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Sidebar />
+
+    <div
+      class="app-content content"
+      :class="{ 'ash-bg': $route.name === 'dashboard' }"
+    >
+      <div class="content-overlay"></div>
+      <div class="header-navbar-shadow"></div>
+      <div class="content-wrapper mt-4">
+        <div class="content-header row"></div>
+        <div class="content-body pb-4">
+          <Nuxt />
+        </div>
+      </div>
+    </div>
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+  </div>
+</template>
+<script>
+import Sidebar from '../components/navs/Sidebar.vue';
+
+export default {
+  components: { Sidebar },
+};
+</script>
+
+<style scoped>
+.app-content {
+  min-height: 100vh;
+  /* overflow: hidden auto; */
+}
+
+.ash-bg {
+  background-color: #f7f8fb;
+}
+</style>
