04e1f3ee3daf9b415b5f1a82d8e693b871eab9e7
sanjaesan <sanusijubril.sj@gmail.com>
2021-08-28 08:43:35 +0100

    dext tools footer link updated

--- components/navs/Footer.vue
+++ components/navs/Footer.vue
@@ -6,7 +6,8 @@
     </div>
     <div class="d-lg-flex justify-content-between text-center">
       <a href="https://twitter.com/cag3_io" target="_blank" class="mx-2 mx-lg-4">Twitter</a>
-      <a href="https://t.me/c4g3Index" target="_blank" class="mx-2 mx-lg-4">Telegram</a>
+       <a href="https://t.me/c4g3Index" target="_blank" class="mx-2 mx-lg-4">Telegram</a>
+      <a href="https://www.dextools.io/app/uniswap/pair-explorer/0xfe2743bcc93033d92c91f7998c44b112325c8ce2" target="_blank" class="mx-2 mx-lg-4">Dextools</a>
       <a href="https://t.me/cag3_Index" target="_blank" class="mx-2 mx-lg-4">Announcement</a>
       <a href="https://docs.cag3.io" target="_blank" class="mx-2 mx-lg-4">Docs</a>
       <a href="https://github.com/C4G3M4ST3R" target="_blank" class="ml-2">Github</a>
