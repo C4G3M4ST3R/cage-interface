0b944ca076272080b219ac9d184a8391ad6cbdd2
Oluwaseyi Joshua <seyijosh44@gmail.com>
2021-08-31 13:02:51 +0100

    Added supply page

---
+++ pages/supply.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="m-3">
+    {{ result }}
+  </div>
+</template>
+
+<script>
+import bcLibrary from '../mixins/bc';
+export default {
+  layout: 'none',
+  mixins: [bcLibrary],
+  data() {
+    return {
+      result: 'Generating...',
+    };
+  },
+  async created() {
+    try {
+      const [{ result }, { result: burned }] = await Promise.all([
+        (
+          await fetch(
+            'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xf2ef3551c1945a7218fc4ec0a75c9ecfdf012a4f',
+          )
+        ).json(),
+        (
+          await fetch(
+            `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xf2ef3551c1945a7218fc4ec0a75c9ecfdf012a4f&address=0x000000000000000000000000000000000000dead&tag=latest&apikey=998ZUNKYQV81292JI8A4MA5B8PQEI7RX3A`,
+          )
+        ).json(),
+      ]);
+
+      let value = this.bcsub(result, burned);
+
+      let power = Math.pow(10, 18);
+      let answer = this.bcdiv(value, power, 18);
+      this.result = answer;
+    } catch (e) {
+      console.log({ e });
+    }
+  },
+};
+</script>
+
+<style></style>
