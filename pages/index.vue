f99a7a6b2733ae0e157c561a18255511b755636f
sanjaesan <sanusijubril.sj@gmail.com>
2021-10-04 13:36:19 +0100

    fixes

--- pages/index.vue
+++ pages/index.vue
@@ -614,7 +614,7 @@
 
     <!--====== BRAND PART ENDS ======-->
 
-    <!--====== TEAM PART START ======-->
+    <!--====== Partner & Advisors PART START ======-->
 
     <section id="team" class="team-area pt-120">
       <div class="container">
@@ -622,7 +622,7 @@
           <div class="col-lg-5">
             <div class="section-title text-center pb-30">
               <div class="line m-auto"></div>
-              <h3 class="title"><span>Meet Our</span> Partners and Advisors</h3>
+              <h3 class="title"><span>Meet Our</span> Partners & Advisors</h3>
             </div>
             <!-- section title -->
           </div>
@@ -640,13 +640,7 @@
                 <div class="social">
                   <ul>
                     <li>
-                      <a href="#"><i class="lni-facebook-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-twitter-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-instagram-filled"></i></a>
+                      <a href="https://twitter.com/NedHalawani"><i class="lni-twitter-filled"></i></a>
                     </li>
                     <li>
                       <a href="#"><i class="lni-linkedin-original"></i></a>
@@ -655,8 +649,8 @@
                 </div>
               </div>
               <div class="team-content">
-                <h5 class="holder-name"><a href="#">Isabela Moreira</a></h5>
-                <p class="text">Founder and CEO</p>
+                <h5 class="holder-name"><a href="#">Mohanned Halawani</a></h5>
+                <p class="text">Founder @ CryptoPR</p>
               </div>
             </div>
             <!-- single team -->
@@ -672,55 +666,14 @@
                 <div class="social">
                   <ul>
                     <li>
-                      <a href="#"><i class="lni-facebook-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-twitter-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-instagram-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-linkedin-original"></i></a>
+                      <a href="https://twitter.com/scrate_io"><i class="lni-twitter-filled"></i></a>
                     </li>
                   </ul>
                 </div>
               </div>
               <div class="team-content">
-                <h5 class="holder-name"><a href="#">Elon Musk</a></h5>
-                <p class="text">Sr. Software Engineer</p>
-              </div>
-            </div>
-            <!-- single team -->
-          </div>
-          <div class="col-lg-4 col-md-7 col-sm-8">
-            <div
-              class="single-team text-center mt-30 wow fadeIn"
-              data-wow-duration="1s"
-              data-wow-delay="0.8s"
-            >
-              <div class="team-image">
-                <img src="/img/images/team-3.png" alt="Team" />
-                <div class="social">
-                  <ul>
-                    <li>
-                      <a href="#"><i class="lni-facebook-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-twitter-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-instagram-filled"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="lni-linkedin-original"></i></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div class="team-content">
-                <h5 class="holder-name"><a href="#">Fiona Smith</a></h5>
-                <p class="text">Business Development Manager</p>
+                <h5 class="holder-name"><a href="#">Scrate.io</a></h5>
+                <p class="text">Africa's Blockchain Educational Platform</p>
               </div>
             </div>
             <!-- single team -->
@@ -731,7 +684,7 @@
       <!-- container -->
     </section>
 
-    <!--====== TEAM PART ENDS ======-->
+    <!--====== Partner & Advisors PART ENDS ======-->
 
     <!--====== ROAD MAP PART START ======-->
 
